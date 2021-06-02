d6ccbfc9942f8d68f67c4b06cd0ce8b95e4f18c2
JonathanTheZero <jonathan.frohne@web.de>
2021-06-02 11:30:20 +0200

    Wiederholungsaufgabe 06/21

---
+++ AW1/Wiederholung/Wiederholung0621.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AW1.Wiederholung
+{
+    public static class Wiederholung0621
+    {
+        public static void TaskA()
+        {
+
+        }
+    }
+}
